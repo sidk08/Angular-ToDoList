56388318b1bfcd4d315c1e988fbe55788c2eca3a
Asthashah <shah.ast@husky.neu.edu>
2018-03-22 11:10:03 -0400

    headerchangesvalidation&footerchanges

--- ng/src/app/footercomponent/footercomponent.component.spec.ts
+++ ng/src/app/footercomponent/footercomponent.component.spec.ts
@@ -1,25 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeadercomponentComponent } from './headercomponent.component';
+import { FootercomponentComponent } from './footercomponent.component';
 
-describe('HeadercomponentComponent', () => {
-  let component: HeadercomponentComponent;
-  let fixture: ComponentFixture<HeadercomponentComponent>;
+describe('FootercomponentComponent', () => {
+  let component: FootercomponentComponent;
+  let fixture: ComponentFixture<FootercomponentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeadercomponentComponent ]
+      declarations: [ FootercomponentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeadercomponentComponent);
+    fixture = TestBed.createComponent(FootercomponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
