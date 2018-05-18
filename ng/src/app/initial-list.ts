0f44733a31e8f33d74dbbe754e0b8471507b93da
Siddhesh Kuvalekar <sid.kuvalekar@gmail.com>
2018-05-18 12:35:16 -0400

    ToDoApp

--- ng/src/app/initial-list.ts
+++ ng/src/app/initial-list.ts
@@ -3,7 +3,7 @@ import {item} from './listitem';
 export const initItems: item[] = [
     {id:1, value: 'get groceries', status: true},
     {id:2, value: 'do laundry', status: true},
-    {id:3, value: 'get a job', status: false},
-    {id:4, value: 'cook food', status: false},
-    {id:5, value: 'prepare for exam', status: false},
+    {id:3, value: 'pay electricity bills', status: false},
+    {id:4, value: 'exercise', status: false},
+    {id:5, value: 'car servicing', status: false},
 ];
